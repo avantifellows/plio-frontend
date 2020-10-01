30583a64f86f3e24f793eaf1e4031b4d6b4b9df2
Pritam Sukumar <pritamps@gmail.com>
2020-10-01 14:41:53 +0530

    Markers to questions

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,12 +1,15 @@
-<template>  
-    {{ $route.params.id }}
-    
-    <div class="player_container" v-if="video_data">
-        {{ video_data }}
+<template>
+  {{ $route.params.id }}
 
-        <div id="player" data-plyr-provider="youtube" :data-plyr-embed-id="video_data.ivideo_details.video_id"></div>
-    </div>
-    
+  <div class="player_container" v-if="video_data">
+    {{ video_data }}
+
+    <div
+      id="player"
+      data-plyr-provider="youtube"
+      :data-plyr-embed-id="video_data.ivideo_details.video_id"
+    ></div>
+  </div>
 </template>
 
 <script>
@@ -19,6 +22,45 @@ export default {
     return {
       video_data: null,
     };
+  },
+  async created() {
+    await this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      axios
+        .get(
+          process.env.VUE_APP_BACKEND_URL +
+            process.env.VUE_APP_BACKEND_IVIDEO_DETAILS +
+            "?ivideo_id=" +
+            this.$route.params.id
+        )
+        .then((res) => (this.video_data = res.data))
+        .then(() => (this.player = new Plyr("#player")))
+        .then(() => this.setPlayerProperties(this.player))
+        .catch((err) => console.log(err));
+    },
+
+    setPlayerProperties(player) {
+      player.on("ready", () => {
+        console.log(this.video_data);
+        var questions = this.video_data.ivideo_details.questions.questions;
+        var progressBar = document.querySelectorAll(".plyr__progress")[0];
+
+        questions.forEach(function (question) {
+          console.log(question.time);
+          var marker = document.createElement("SPAN");
+          marker.className = "plyr__tooltip";
+          marker.style.setProperty("opacity", 1);
+          marker.style.setProperty("background-color", "#ff0");
+          marker.style.setProperty("bottom", "80%");
+          marker.style.setProperty("height", "80%");
+          var pos_percent = (question.time / player.duration) * 100;
+          marker.style.setProperty("left", `${pos_percent}%`);
+          progressBar.appendChild(marker);
+        });
+      });
+    },
   },
   computed: {
     playerOptions() {
@@ -34,26 +76,20 @@ export default {
   },
   mounted() {
     // Store in data
-    console.log(process.env.VUE_APP_BACKEND_URL +
-          process.env.VUE_APP_BACKEND_IVIDEO_DETAILS +
-          "?ivideo_id=" + this.$route.params.id)
-    axios
-      .get(
-        process.env.VUE_APP_BACKEND_URL +
-          process.env.VUE_APP_BACKEND_IVIDEO_DETAILS +
-          "?ivideo_id=" + this.$route.params.id
-      )
-      .then((res) => (this.video_data = res.data ))
-      .then( () => this.player = new Plyr("#player"))
-      .catch((err) => console.log(err));
-    
+    console.log(
+      process.env.VUE_APP_BACKEND_URL +
+        process.env.VUE_APP_BACKEND_IVIDEO_DETAILS +
+        "?ivideo_id=" +
+        this.$route.params.id
+    );
   },
 };
 </script>
 
 <style>
 @import "https://cdn.plyr.io/3.6.2/plyr.css";
-#player {
-    height: 300px;
+.player_container {
+  width: 800px;
+  margin: auto;
 }
 </style>
