f78c2a76a32e8d44ba9ca32ba32af3adb80208d2
Pritam Sukumar <pritamps@gmail.com>
2020-10-08 17:19:38 +0530

    Adding phone input, clearing timeout

---
+++ src/views/PhoneSignIn.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    <div class="lead_text">
+      <p>कृपया अपना मोबाइल नंबर डालें</p>
+      <p>Please enter your mobile number (10 digits only)</p>
+    </div>
+    <input id="phone" v-model="phone_input" type="phone" maxlength="10" />
+    <div class="watch_plio">
+      <button id="submit" :disabled="isSubmitDisabled" @click="storePhone">
+        Watch Plio!
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import PhoneNumber from "awesome-phonenumber";
+
+export default {
+  data() {
+    return {
+      phone_input: "",
+      isSubmitDisabled: true,
+    };
+  },
+  watch: {
+    phone_input: function () {
+      let phone = PhoneNumber(this.phone_input.toString(), "IN");
+      console.log(phone.isValid());
+      if (!phone.isValid()) {
+        this.isSubmitDisabled = true;
+      } else {
+        this.isSubmitDisabled = false;
+      }
+    },
+  },
+  methods: {
+    storePhone() {
+      this.$store
+        .dispatch("login", {
+          phone: this.phone_input,
+        })
+        .then(() => {
+          if (this.$route.params.id) {
+            console.log("id:" + this.$route.params.id);
+          } else {
+            this.$router.push({ path: "/" });
+          }
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.lead_text {
+  font-size: 1.6em;
+  font-weight: bold;
+}
+
+.container {
+  position: fixed;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+#phone {
+  margin: auto;
+  max-width: 300px;
+  font-size: 1.6em;
+}
+
+button {
+  background-color: #df7c3a;
+  border: none;
+  color: white;
+  padding: 10px 32px;
+  text-align: center;
+  border-radius: 5px;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 32px 2px;
+  cursor: pointer;
+}
+
+button:disabled {
+  background-color: grey;
+}
+
+button:disabled:hover {
+  cursor: pointer;
+}
+</style>
