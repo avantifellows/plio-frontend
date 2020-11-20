858fa690e475d5df373aba7ec65c44ac8e528336
Aman Dalmia <amandalmia18@gmail.com>
2020-11-20 18:33:08 +0530

    FIX: added indexOf function for older browsers

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -32,6 +32,30 @@ import axios from "axios";
 import PlioQuestion from "../components/PlioQuestion.vue";
 import Error from "../views/Error.vue";
 
+// supports indexOf for older browsers
+if (!Array.prototype.indexOf)
+{
+  Array.prototype.indexOf = function(elt /*, from*/)
+  {
+    var len = this.length >>> 0;
+
+    var from = Number(arguments[1]) || 0;
+    from = (from < 0)
+         ? Math.ceil(from)
+         : Math.floor(from);
+    if (from < 0)
+      from += len;
+
+    for (; from < len; from++)
+    {
+      if (from in this &&
+          this[from] === elt)
+        return from;
+    }
+    return -1;
+  };
+}
+
 // The time period in which Plyr timeupdate event repeats
 // in milliseconds
 var interval_time = 50;
