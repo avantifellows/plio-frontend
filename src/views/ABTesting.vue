8c3850b09d0eb300ae46ef2ce437a122bf7e92e8
Aman Dalmia <amandalmia18@gmail.com>
2021-01-16 12:06:12 +0530

    NEW: v1 of a/b testing done

--- src/views/ABTesting.vue
+++ src/views/ABTesting.vue
@@ -3,35 +3,66 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: "ABTesting",
+    data() {
+        return {
+            assignment: null,
+            userConfig: {},
+            plioId: null
+        };
+    },
 
     created() {
         if(!localStorage.phone) {
             this.$router.push({
                 path: '/login/' + this.$route.params.id + '/experiment'
             })
+        } else {
+            // get variant and redirect user
+            this.getAssignment();
         }
-        
-        var experimentAssignment = this.getAssignment()
-        var assignment = experimentAssignment[0]
-        var userConfig = experimentAssignment[1]
-
-        this.$store
-            .dispatch("saveConfig", {
-                config: userConfig,
-            }).then(() => {
-                this.$router.push({
-                    path: '/play/' + assignment,
-                    query: {
-                        'experiment': this.$route.params.id
-                    }
-                })
-            })
     },
     methods: {
         getAssignment() {
-            return ["ZIzADHngzy", {}];
+            var url = process.env.VUE_APP_BACKEND + 
+                process.env.VUE_APP_BACKEND_EXPERIMENT_ASSIGNMENT +
+                "?experimentId=" + this.$route.params.id +
+                "&userId=" + localStorage.phone;
+            axios
+                .get(url)
+                .then((res) => {
+                    // separately seting plio ID although it will be the
+                    // same as assignment for now as we might conduct interface
+                    // level changes where assignment won't be the same as plio ID
+                    this.assignment = res.data.assignment;
+                    this.userConfig = res.data.config;
+                    this.plioId = res.data.plioId;
+                })
+                .then(() => this.$store.dispatch("saveConfig", {
+                        config: this.userConfig,
+                    })
+                )
+                .then(() => {
+                    console.log('Assignment: ' + this.assignment)
+                    this.$router.push({
+                        path: '/play/' + this.plioId,
+                        query: {
+                            'experiment': this.$route.params.id
+                        }
+                    })
+                })
+                .catch((err) => this.handleQueryError(err));
+        },
+
+        handleQueryError(err) {
+            if (err.response && err.response.status == 404) {
+                this.$router.push('/404-not-found')
+            } else { 
+                console.log(err)
+            }
         }
     }
 
