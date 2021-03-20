e81d19846d9114249d3fefc43f584c4d0ccbf134
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-03-20 13:55:47 +0530

    WIP: setting up primevue and tailwind

--- src/main.js
+++ src/main.js
@@ -5,10 +5,17 @@ import store from "./store";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import i18n from "./i18n";
+import PrimeVue from "primevue/config";
+
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "tailwindcss/tailwind.css";
 
 const app = createApp(App).use(store).use(router);
 
 app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(i18n);
+app.use(PrimeVue, { ripple: true });
 app.mount("#app");
