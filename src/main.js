4791c4136479e82ca8063b66e2b84f9df5bae22c
deepansh96 <deepansh.mathur96@gmail.com>
2021-03-22 10:05:36 +0530

    Update src/main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from "./store";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import i18n from "./services/Localisation/i18n.js";
 import PrimeVue from "primevue/config";
-
+import "primevue/resources/themes/saga-blue/theme.css";
 import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import "./index.css";
