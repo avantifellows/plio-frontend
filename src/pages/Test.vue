31b64e35282ab4053e8626a3c574012472c18a9f
Aman Dalmia <amandalmia18@gmail.com>
2021-03-29 18:40:15 +0530

    FIX: add back browser close check

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,22 +1,14 @@
 <template>
   <div>
-    <dialog-box
-      title="Are you sure you want to leave?"
-      @confirm="confirmClicked"
-    ></dialog-box>
+    <circle-spinner></circle-spinner>
   </div>
 </template>
 
 <script>
-import DialogBox from "@/components/UI/Alert/DialogBox";
+import CircleSpinner from "@/components/UI/Spinner/CircleSpinner";
 export default {
   components: {
-    DialogBox,
-  },
-  methods: {
-    confirmClicked() {
-      console.log("confirm");
-    },
+    CircleSpinner,
   },
 };
 </script>
