01403e7830d4f4a216f77f298f2fde108bcdf734
Aman Dalmia <amandalmia18@gmail.com>
2021-03-29 18:47:06 +0530

    FIX: move test.vue back to how it was

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <circle-spinner></circle-spinner>
+    <dialog-box
+      title="Are you sure you want to leave?"
+      @confirm="confirmClicked"
+    ></dialog-box>
   </div>
 </template>
 
 <script>
-import CircleSpinner from "@/components/UI/Spinner/CircleSpinner";
+import DialogBox from "@/components/UI/Alert/DialogBox";
 export default {
   components: {
-    CircleSpinner,
+    DialogBox,
+  },
+  methods: {
+    confirmClicked() {
+      console.log("confirm");
+    },
   },
 };
 </script>
