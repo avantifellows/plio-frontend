bc187e4d28a30ec08ac6f64f7f61184d722247d3
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-04-08 15:20:06 +0530

    bug

--- src/pages/Editor.vue
+++ src/pages/Editor.vue
@@ -859,10 +859,11 @@ export default {
         .then((createdItem) => {
           // storing the newly created item into "newItem"
           newItem = createdItem;
-          if (createdItem.type == "question")
+          if (createdItem.type == "question"){
             var questionDetails = this.getDetailsForNewQuestion();
             questionDetails.item = createdItem.id;
             return QuestionAPIService.createQuestion(questionDetails);
+          }
         })
         .then((createdQuestion) => {
           // storing the newly created question into "newItem"
