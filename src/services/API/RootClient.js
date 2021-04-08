bc9fc19e734b83b4f72c6711a14a4d1355e76f61
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-04-08 19:27:26 +0530

    access token

--- src/services/API/RootClient.js
+++ src/services/API/RootClient.js
@@ -1,12 +1,20 @@
 import axios from "axios";
+import store from "../../store";
+
+let headers = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+if (store.state.auth && store.state.auth.accessToken) {
+  headers[
+    "Authorization"
+  ] = `Bearer ${store.state.auth.accessToken.access_token}`;
+}
 
 const client = axios.create({
   baseURL: process.env.VUE_APP_BACKEND,
-  withCredentials: false, //default
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+  withCredentials: false,
+  headers,
 });
 
 export function apiClient() {
