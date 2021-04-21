90da99172c5e7675c103fc66e2a9f159a12a60ab
deepansh96 <deepansh.mathur96@gmail.com>
2021-04-21 19:29:03 +0530

    Update src/services/Functional/ErrorHandling.js

--- src/services/Functional/ErrorHandling.js
+++ src/services/Functional/ErrorHandling.js
@@ -4,7 +4,6 @@ const toast = useToast();
 export default {
   handleAPIErrors(error) {
     if (!error.response) {
-      // https://github.com/axios/axios/issues/383
       toast.error("Please check your internet connection.");
     }
   },
