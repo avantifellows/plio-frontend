8caed5e8aa42cb7987a90c1b0cb3bea826a4502c
Aman Dalmia <amandalmia18@gmail.com>
2021-05-24 20:41:26 +0530

    NEW: video skeleton

--- src/components/UI/Skeletons/VideoSkeleton.vue
+++ src/components/UI/Skeletons/VideoSkeleton.vue
@@ -1,24 +1,14 @@
 <template>
-  <div class="border border-primary shadow p-4 m-4">
-    <div class="animate-pulse flex flex-col text-primary">
-      <div class="flex justify-center">
-        <inline-svg
-          :src="playIcon"
-          class="fill-current text-primary w-1/6 h-1/6 m-10 sm:m-20 lg:m-36 place-self-center"
-        ></inline-svg>
-      </div>
-      <div class="h-4 bg-primary rounded"></div>
+  <div class="shadow p-4 m-4">
+    <div class="animate-pulse flex flex-col">
+      <div class="flex justify-center bg-gray-600 w-full h-96 my-4"></div>
+      <div class="h-4 bg-gray-400 rounded"></div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    playIcon() {
-      // fetches and returns the play icon
-      return require("@/assets/images/play.svg");
-    },
-  },
+  name: "VideoSkeleton",
 };
 </script>
