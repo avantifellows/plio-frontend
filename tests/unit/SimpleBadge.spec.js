783c43315cbb8add250804dd2f0faf3b4d2a7b69
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-06-14 22:41:10 +0530

    test/utils instead of testing-library

---
+++ tests/unit/SimpleBadge.spec.js
@@ -0,0 +1,26 @@
+import { shallowMount } from "@vue/test-utils";
+import SimpleBadge from "@/components/UI/Badges/SimpleBadge";
+
+describe("SimpleBadge.vue", () => {
+  it("renders title config correctly", () => {
+    const badgeText = "Test Badge";
+    const wrapper = shallowMount(SimpleBadge, {
+      props: {
+        text: badgeText,
+      },
+    });
+    expect(wrapper.text()).toMatch(badgeText);
+  });
+
+  it("renders css class correctly", () => {
+    const badgeText = "Test Badge";
+    const badgeClass = "text-white";
+    const wrapper = shallowMount(SimpleBadge, {
+      props: {
+        text: badgeText,
+        badgeClass: badgeClass,
+      },
+    });
+    expect(wrapper.find("span").classes()).toContain(badgeClass);
+  });
+});
