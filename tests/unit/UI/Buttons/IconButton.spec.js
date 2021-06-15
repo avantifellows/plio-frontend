d84055fa28c0d9312283ba99fa3216f0b499986c
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-06-15 17:38:11 +0530

    removed warnings

--- tests/unit/UI/Buttons/IconButton.spec.js
+++ tests/unit/UI/Buttons/IconButton.spec.js
@@ -1,10 +1,16 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import IconButton from "@/components/UI/Buttons/IconButton";
+import { config } from "@vue/test-utils";
+import InlineSvg from "vue-inline-svg";
+
+config.global.components = {
+  InlineSvg,
+};
 
 describe("IconButton.vue", () => {
   it("renders title correctly", () => {
     const buttonText = "Test Button";
-    const wrapper = shallowMount(IconButton, {
+    const wrapper = mount(IconButton, {
       props: {
         titleConfig: {
           value: buttonText,
