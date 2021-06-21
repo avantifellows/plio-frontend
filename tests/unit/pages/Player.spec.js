2b2cfba40e8b19f4d3ff5cf9ecb8e74f843b1351
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-06-21 14:43:06 +0530

    WIP: env variable is accessible

--- tests/unit/pages/Player.spec.js
+++ tests/unit/pages/Player.spec.js
@@ -4,9 +4,6 @@ import Player from "@/pages/Player.vue";
 describe("Player.vue", () => {
   it("renders properly with default values", () => {
     const wrapper = mount(Player);
-    console.log(process.env.VUE_APP_MIXPANEL_PROJECT_TOKEN);
-    console.log(env.VUE_APP_MIXPANEL_PROJECT_TOKEN);
-    console.log(wrapper.vm.$mixpanel);
     expect(wrapper).toBeTruthy();
   });
 });
