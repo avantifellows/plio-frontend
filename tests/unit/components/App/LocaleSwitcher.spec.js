341344fab3df06892a68cb1f862047833c30d103
Aman Dalmia <amandalmia18@gmail.com>
2021-06-24 15:11:10 +0530

    NEW: test triggering localeswitcher chagne

--- tests/unit/components/App/LocaleSwitcher.spec.js
+++ tests/unit/components/App/LocaleSwitcher.spec.js
@@ -6,4 +6,15 @@ describe("LocaleSwitcher.vue", () => {
     const wrapper = mount(LocaleSwitcher);
     expect(wrapper).toBeTruthy();
   });
+
+  it("test updating switcher triggers updating the locale", () => {
+    const updateLocale = jest.spyOn(LocaleSwitcher.methods, "updateLocale");
+    const wrapper = mount(LocaleSwitcher);
+
+    const select = wrapper.find('[data-test="select"]').element;
+    select.value = "hi";
+    select.dispatchEvent(new Event("change"));
+
+    expect(updateLocale).toHaveBeenCalled();
+  });
 });
