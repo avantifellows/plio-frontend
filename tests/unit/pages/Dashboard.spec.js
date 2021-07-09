fdeb3ae9302362e05063e554719bfa948bc91a2a
Aman Dalmia <amandalmia18@gmail.com>
2021-07-09 12:04:08 +0530

    WIP

--- tests/unit/pages/Dashboard.spec.js
+++ tests/unit/pages/Dashboard.spec.js
@@ -1,9 +1,22 @@
 import { mount } from "@vue/test-utils";
+import axios from "axios";
 import Dashboard from "@/pages/Dashboard.vue";
+
+// Following lines tell Jest to mock any call to `axios.get`
+jest.mock("axios", () => ({
+  get: jest.fn(),
+}));
 
 describe("Dashboard.vue", () => {
   it("renders properly with default values", () => {
-    const wrapper = mount(Dashboard);
+    const wrapper = mount(Dashboard, {
+      props: {
+        plioId: "abc",
+      },
+    });
     expect(wrapper).toBeTruthy();
+    expect(axios.get).toHaveBeenCalledTimes(2);
+
+    console.log(wrapper.vm.videoID);
   });
 });
