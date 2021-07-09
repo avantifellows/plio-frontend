7481b0634ed473e22e1f56bd4ad8c710cae20bbc
Aman Dalmia <amandalmia18@gmail.com>
2021-07-09 13:50:44 +0530

    FIX: axios mock

--- tests/unit/pages/Dashboard.spec.js
+++ tests/unit/pages/Dashboard.spec.js
@@ -1,16 +1,29 @@
-import { mount } from "@vue/test-utils";
+import { mount, flushPromises } from "@vue/test-utils";
+import mockAxios from "jest-mock-axios";
+
 import Dashboard from "@/pages/Dashboard.vue";
+import { dummyPlio, dummyItems } from "@/services/Testing/DummyData.js";
+
+afterEach(() => {
+  // cleaning up the mess left behind by the previous test
+  mockAxios.reset();
+});
 
 describe("Dashboard.vue", () => {
-  it("renders properly with default values", () => {
+  it("renders properly for a given Plio", () => {
+    const plioId = "abc";
     const wrapper = mount(Dashboard, {
       props: {
-        plioId: "abc",
+        plioId: plioId,
       },
     });
     expect(wrapper).toBeTruthy();
-    // expect(axios.get).toHaveBeenCalledTimes(2);
-
-    // console.log(wrapper.vm.videoID);
+    // `getPlio` inside services/API/Plio.js should've been called
+    // 2 `GET` requests are made
+    expect(mockAxios.get).toHaveBeenCalledTimes(2);
+    expect(mockAxios.get).toHaveBeenCalledWith(`/plios/${plioId}`);
+    expect(mockAxios.get).toHaveBeenCalledWith("/items/", {
+      params: { plio: `${plioId}` },
+    });
   });
 });
