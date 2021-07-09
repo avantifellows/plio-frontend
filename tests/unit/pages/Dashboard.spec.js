efe9de3319050d3ea9d1cfa4a9e7aec7001dc897
Aman Dalmia <amandalmia18@gmail.com>
2021-07-09 13:28:27 +0530

    FIX: revert changes

--- tests/unit/pages/Dashboard.spec.js
+++ tests/unit/pages/Dashboard.spec.js
@@ -1,11 +1,5 @@
 import { mount } from "@vue/test-utils";
-import axios from "axios";
 import Dashboard from "@/pages/Dashboard.vue";
-
-// Following lines tell Jest to mock any call to `axios.get`
-jest.mock("axios", () => ({
-  get: jest.fn(),
-}));
 
 describe("Dashboard.vue", () => {
   it("renders properly with default values", () => {
@@ -15,8 +9,8 @@ describe("Dashboard.vue", () => {
       },
     });
     expect(wrapper).toBeTruthy();
-    expect(axios.get).toHaveBeenCalledTimes(2);
+    // expect(axios.get).toHaveBeenCalledTimes(2);
 
-    console.log(wrapper.vm.videoID);
+    // console.log(wrapper.vm.videoID);
   });
 });
