7f35237acf9f7d576f04dd7ae1d692f8bfb0e7e5
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-09-01 15:51:20 +0530

    editor test spec

--- tests/unit/pages/Editor.spec.js
+++ tests/unit/pages/Editor.spec.js
@@ -171,22 +171,17 @@ describe("Editor.vue", () => {
       },
     });
     // `getPlio` inside services/API/Plio.js should've been called
-    // 2 `GET` requests are made
-    expect(mockAxios.get).toHaveBeenCalledTimes(2);
+    // 1 `GET` request is made
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
     expect(mockAxios.get).toHaveBeenCalledWith(`/plios/${plioId}`);
-    expect(mockAxios.get).toHaveBeenCalledWith("/items/", {
-      params: { plio: `${plioId}` },
-    });
 
     // using some pre-defined dummy data to return as a fake response
     // from the fake API call
     let plioResponse = dummyDraftPlio;
-    let itemResponse = dummyItemsCopy;
 
-    // resolve the two `GET` requests waiting in the queue
+    // resolve the `GET` request waiting in the queue
     // using the fake response data
     mockAxios.mockResponse(plioResponse, mockAxios.queue()[0]);
-    mockAxios.mockResponse(itemResponse, mockAxios.queue()[1]);
 
     // wait until the DOM updates after promises resolve
     await flushPromises();
