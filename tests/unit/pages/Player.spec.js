509853d335a91a2775dfa9e7b64a1468d22969fe
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-09-23 18:08:34 +0530

    FIX: fixed deepscan error

--- tests/unit/pages/Player.spec.js
+++ tests/unit/pages/Player.spec.js
@@ -1,7 +1,6 @@
-import { flushPromises, mount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 import Player from "@/pages/Player.vue";
 import mockAxios from "jest-mock-axios";
-import { dummyAccessToken, dummyItems } from "@/services/Testing/DummyData.js";
 
 afterEach(() => {
   // cleaning up the mess left behind by the previous test
