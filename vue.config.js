f3236965420ea8fa57acca394d16eedd562458da
Aman Dalmia <amandalmia18@gmail.com>
2021-10-22 12:58:01 +0530

    FIX: enable source-map for debugger

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,8 @@ module.exports = {
     // to enable compression of assets: https://web.dev/uses-text-compression/?utm_source=lighthouse&utm_medium=devtools
     config.plugin("CompressionPlugin").use(CompressionPlugin);
   },
+  configureWebpack: {
+    // lets debugger map the code within a compressed file back to its position in the original file
+    devtool: "source-map",
+  },
 };
