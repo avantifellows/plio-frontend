9d1454c264d821e192f36bd134814cd8edfdd16c
Aman Dalmia <amandalmia18@gmail.com>
2021-10-26 14:57:46 +0530

    FIX: added transpiling for some modules + prevent prefetching of all chunks

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,9 @@ module.exports = {
     // lets debugger map the code within a compressed file back to its position in the original file
     devtool: "source-map",
   },
+  transpileDependencies: [/@vue\/*/, "vue-router", "plyr"],
+  chainWebpack(config) {
+    // reference: https://medium.com/@aetherus.zhou/vue-cli-3-performance-optimization-55316dcd491c
+    config.plugins.delete("prefetch");
+  },
 };
