31369c90a4796590f3b74769fc5d2e12956d22a2
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-11-12 16:55:35 +0530

    some cleanups

--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -1,27 +1,16 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
-
 describe('sees login', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('http://localhost:8080/')
   });
 
   it('sees the google sign in option', () => {
-    cy.get('[data-test="googleLogin"]').should('have.text', 'Sign in with Google');
+    cy.get('[data-test="googleLogin"] p').should('have.text', 'Sign in with Google');
     // cy.get('[data-test="googleLogin"] p').should('have.text', 'गूगल से साइन इन करें');
+  });
+
+  it('sees the language switcher dropdown', () => {
+    cy.get('#locale > select').should('be.visible');
   });
 });
