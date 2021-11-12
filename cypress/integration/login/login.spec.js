30a52ba26a1f421ffe8d05b9b6ef5343e58d6383
Vaibhav Rathore <vaibhav@coloredcow.com>
2021-11-12 16:35:57 +0530

    package updates

--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -18,20 +18,10 @@ describe('sees login', () => {
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
     cy.visit('http://localhost:8080/')
-  })
+  });
 
   it('sees the google sign in option', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
-    // cy.get('[data-test="googleLogin"]').should('have.text', 'Sign in with Google');
-    cy.get('[data-test="googleLogin"] p').should('have.text', 'गूगल से साइन इन करें');
-
-    // // We can go even further and check that the default todos each contain
-    // // the correct text. We use the `first` and `last` functions
-    // // to get just the first and last matched elements individually,
-    // // and then perform an assertion with `should`.
-    // cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
+    cy.get('[data-test="googleLogin"]').should('have.text', 'Sign in with Google');
+    // cy.get('[data-test="googleLogin"] p').should('have.text', 'गूगल से साइन इन करें');
   });
 });
