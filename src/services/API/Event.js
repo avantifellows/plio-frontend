1db52ffa41a409fd8d3275cc581599b511737a0c
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-11-24 17:39:42 +0530

    FIX: increased upload interval + watching events will be updated instead of re-created everytime

--- src/services/API/Event.js
+++ src/services/API/Event.js
@@ -2,6 +2,10 @@ import { apiClient } from "@/services/API/RootClient.js";
 import { eventsEndpoint } from "@/services/API/Endpoints.js";
 
 export default {
+  /**
+   *
+   * @param {Object} eventData - The data that will be used as a payload in the POST request
+   */
   createEvent(eventData) {
     // creates a new event
     return apiClient()
@@ -9,5 +13,13 @@ export default {
       .then((response) => {
         return response.data;
       });
+  },
+  /**
+   *
+   * @param {Number} eventId - The ID of the event that needs to be patched
+   * @param {Object} eventData - The event will be patched with this data
+   */
+  updateEvent(eventId, eventData) {
+    return apiClient().patch(eventsEndpoint + eventId, eventData);
   },
 };
