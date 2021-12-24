6623b1f9578d80b7c1dea147d51e6760d3a29b0d
Deepansh Mathur <deepansh.mathur96@gmail.com>
2021-12-24 15:00:35 +0530

    Tests for settings

--- tests/unit/components/Collections/Settings/Settings.spec.js
+++ tests/unit/components/Collections/Settings/Settings.spec.js
@@ -1,38 +1,45 @@
 import { mount } from "@vue/test-utils";
 import Settings from "@/components/Collections/Settings/Settings.vue";
 
-let clonedeep = require('lodash.clonedeep')
+let clonedeep = require("lodash.clonedeep");
 
 describe("PlioListItem.vue", () => {
   let wrapper;
-  
+
   it("should render with default values", () => {
     wrapper = mount(Settings);
     expect(wrapper).toBeTruthy();
   });
 
   it("should render provided settings correctly", () => {
-    const setCurrentSelectedTab = jest.spyOn(Settings.methods, 'setCurrentSelectedTab')
+    const setCurrentSelectedTab = jest.spyOn(
+      Settings.methods,
+      "setCurrentSelectedTab"
+    );
     wrapper = mount(Settings, {
       props: {
-        settings: clonedeep(global.dummySettingsToRender)
-      }
-    })
-    expect(setCurrentSelectedTab).toHaveBeenCalled()
-    expect(wrapper.vm.localSettings).toEqual(global.dummySettingsToRender)
+        settings: clonedeep(global.dummySettingsToRender),
+      },
+    });
+    expect(setCurrentSelectedTab).toHaveBeenCalled();
+    expect(wrapper.vm.localSettings).toEqual(global.dummySettingsToRender);
     expect(wrapper.vm.currentSelectedTab).toEqual({
-      configuration: global.dummySettingsToRender.player.configuration
-    })
-    expect(wrapper.vm.hasUnsavedChanges).toBeFalsy()
-    expect(wrapper.vm.currentSelectedTabName).toBe('configuration')
-    expect(wrapper.vm.currentSelectedTabDetails).toStrictEqual(global.dummySettingsToRender.player.configuration)
-    expect(wrapper.find('[data-test="header-player"]').exists()).toBeTruthy()
-    expect(wrapper.find('[data-test="tab-configuration"]').exists()).toBeTruthy()
-    expect(wrapper.find('[data-test="info-message"]').exists()).toBeFalsy()
-  })
+      configuration: global.dummySettingsToRender.player.configuration,
+    });
+    expect(wrapper.vm.hasUnsavedChanges).toBeFalsy();
+    expect(wrapper.vm.currentSelectedTabName).toBe("configuration");
+    expect(wrapper.vm.currentSelectedTabDetails).toStrictEqual(
+      global.dummySettingsToRender.player.configuration
+    );
+    expect(wrapper.find('[data-test="header-player"]').exists()).toBeTruthy();
+    expect(
+      wrapper.find('[data-test="tab-configuration"]').exists()
+    ).toBeTruthy();
+    expect(wrapper.find('[data-test="info-message"]').exists()).toBeFalsy();
+  });
 
   it("sets the clicked tab as selected", async () => {
-    const selectTab = jest.spyOn(Settings.methods, 'selectTab')
+    const selectTab = jest.spyOn(Settings.methods, "selectTab");
     wrapper = mount(Settings, {
       props: {
         settings: {
@@ -43,45 +50,48 @@ describe("PlioListItem.vue", () => {
                 title: "",
                 subTitle: null,
                 type: "checkbox",
-              }
-            }
-          }
-        }
-      }
-    })
+              },
+            },
+          },
+        },
+      },
+    });
 
-    await wrapper.get('[data-test="tab-appearance"]').trigger("click")
-    expect(selectTab).toHaveBeenCalledWith("appearance", wrapper.vm.settings.app.appearance)
+    await wrapper.get('[data-test="tab-appearance"]').trigger("click");
+    expect(selectTab).toHaveBeenCalledWith(
+      "appearance",
+      wrapper.vm.settings.app.appearance
+    );
     expect(wrapper.vm.currentSelectedTab).toStrictEqual({
-      appearance: wrapper.vm.settings.app.appearance
-    })
-  })
+      appearance: wrapper.vm.settings.app.appearance,
+    });
+  });
 
   it("emits any changes made when save is clicked", async () => {
-    const saveChanges = jest.spyOn(Settings.methods, 'saveChanges')
+    const saveChanges = jest.spyOn(Settings.methods, "saveChanges");
     wrapper = mount(Settings, {
       props: {
-        settings: clonedeep(global.dummySettingsToRender)
-      }
-    })
+        settings: clonedeep(global.dummySettingsToRender),
+      },
+    });
 
-    await wrapper.get('[data-test="setting-input"]').trigger("click")
-    await wrapper.get('[data-test="saveButton"]').trigger("click")
-    expect(saveChanges).toHaveBeenCalled()
-    expect(wrapper.emitted()).toHaveProperty("update:settings")
-    expect(wrapper.emitted()).toHaveProperty("window-closed")
-  })
+    await wrapper.get('[data-test="setting-input"]').trigger("click");
+    await wrapper.get('[data-test="saveButton"]').trigger("click");
+    expect(saveChanges).toHaveBeenCalled();
+    expect(wrapper.emitted()).toHaveProperty("update:settings");
+    expect(wrapper.emitted()).toHaveProperty("window-closed");
+  });
 
   it("emits close signal when cancel button is clicked", async () => {
-    const closeMenu = jest.spyOn(Settings.methods, 'closeMenu')
+    const closeMenu = jest.spyOn(Settings.methods, "closeMenu");
     wrapper = mount(Settings, {
       props: {
-        settings: clonedeep(global.dummySettingsToRender)
-      }
-    })
+        settings: clonedeep(global.dummySettingsToRender),
+      },
+    });
 
-    await wrapper.get('[data-test="cancelButton"]').trigger("click")
-    expect(closeMenu).toHaveBeenCalled()
-    expect(wrapper.emitted()).toHaveProperty("window-closed")
-  })
-})
+    await wrapper.get('[data-test="cancelButton"]').trigger("click");
+    expect(closeMenu).toHaveBeenCalled();
+    expect(wrapper.emitted()).toHaveProperty("window-closed");
+  });
+});
