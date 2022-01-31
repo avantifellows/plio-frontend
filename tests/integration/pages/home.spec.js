1356a7a28e9e02f11af38215ec048cccd96016c4
Aman Dalmia <amandalmia18@gmail.com>
2022-01-31 13:32:16 +0530

    WIP: remove if-else check in home tests

--- tests/integration/pages/home.spec.js
+++ tests/integration/pages/home.spec.js
@@ -9,12 +9,15 @@ fixture("Home - English")
 
 test("sees the home page", async (testcafe) => {
   const noPlioSection = Selector('[data-test="noPlio"]');
-  const plioTable = Selector('[data-test="table"]');
-  const noPliosExist = await noPlioSection.exists;
+  await testcafe.expect(noPlioSection.visible).ok();
 
-  if (noPliosExist)
-    // plio table should not exist as there are no plios
-    await testcafe.expect(plioTable.exists).notOk();
-  // plio table should exist as there are plios
-  else await testcafe.expect(plioTable.exists).ok();
+  const plioTable = Selector('[data-test="table"]');
+  await testcafe.expect(plioTable.exists).notOk();
+  // const noPliosExist = await noPlioSection.exists;
+
+  // if (noPliosExist)
+  //   // plio table should not exist as there are no plios
+  //   await testcafe.expect(plioTable.exists).notOk();
+  // // plio table should exist as there are plios
+  // else await testcafe.expect(plioTable.exists).ok();
 });
