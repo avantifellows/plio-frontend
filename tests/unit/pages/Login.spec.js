446b32461fcdf725dbdd9cb4b3d63b2932789bb2
Saloni Choudhary <73693628+Salonichoudhary07@users.noreply.github.com>
2022-02-07 10:24:43 +0000

    test_updated

--- tests/unit/pages/Login.spec.js
+++ tests/unit/pages/Login.spec.js
@@ -113,9 +113,6 @@ describe("Login.vue", () => {
 
         expect(requestOtp).toHaveBeenCalled();
         expect(startResendOTPTimer).toHaveBeenCalled();
-        expect(wrapper.find('[data-test="resendOTP"]').text()).toBe(
-          "OTP दोबारा भेजो"
-        );
         expect(wrapper.vm.resendOTPTimer).toBe(60);
         expect(wrapper.vm.resentOtp).toBe(true);
         expect(wrapper.vm.invalidOtp).toBe(false);
