433da49457924dc05fbd1e5edba63c0cb67612f8
Saloni Choudhary <73693628+Salonichoudhary07@users.noreply.github.com>
2022-02-18 10:34:10 +0000

    survey checkbox disabled when published

--- src/components/Editor/ItemEditor.vue
+++ src/components/Editor/ItemEditor.vue
@@ -93,6 +93,7 @@
               type="checkbox"
               class="form-checkbox h-5 w-5 text-primary focus:ring-transparent"
               v-model="isThisSurveyQuestion"
+              :disabled="isInteractionDisabled"
               data-test="surveyquestioncheckbox"
             /><span class="ml-2 text-gray-700">{{
               $t("editor.item_editor.survey_mode")
