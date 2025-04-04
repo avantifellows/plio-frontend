1f2eb102234e6c1693024aeae1c146774eec506f
ravisinghal033 <ravi.singhal033@gmail.com>
2025-04-05 00:45:03 +0530

    refactor: reuse create plio button via reusable component

---
+++ src/components/CreatePlioButton.vue
@@ -0,0 +1,36 @@
+<!-- src/components/CreatePlioButton.vue -->
+<template>
+  <div>
+    <!-- Desktop version -->
+    <icon-button
+      v-if="!onMobile"
+      :titleConfig="titleConfig"
+      :buttonClass="buttonClass"
+      class="my-4 border-b-outset border-primary hidden bp-500:inline"
+      @click="handleClick"
+      :isDisabled="isDisabled"
+    />
+    <!-- Mobile version -->
+    <icon-button
+      v-else
+      :titleConfig="titleConfig"
+      :buttonClass="buttonClass"
+      class="bp-500:hidden"
+      @click="handleClick"
+      :isDisabled="isDisabled"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  onMobile: {
+    type: Boolean,
+    default: false,
+  },
+  titleConfig: Object,
+  buttonClass: String,
+  isDisabled: Boolean,
+  handleClick: Function,
+});
+</script>
